3008584cbc3b3fde347da2ca8484f2b4177c387b
PriyanshuSwain <53527884+PriyanshuSwain@users.noreply.github.com>
2019-08-07 18:48:59 +0530

    Update demo.cpp

--- demo.cpp
+++ demo.cpp
@@ -1,1 +1,23 @@
-vjghb
+#include<iostream.h>
+#include<conio.h>
+void sum()
+{ int x;
+   x=999;
+  long int y;
+long int sum=0;
+for(int i=0;i<=x;i++)
+  {
+  if((i%3==0)||(i%5==0) )
+   {
+   sum=sum+i;
+   }
+  }
+  y=sum;
+  cout<<y;
+}
+
+void main()
+{           clrscr();
+sum();
+getch();
+}
