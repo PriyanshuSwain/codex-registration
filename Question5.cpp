44124abb25afbe210040b69408571a9d2bf02093
PriyanshuSwain <53527884+PriyanshuSwain@users.noreply.github.com>
2019-09-07 20:48:24 +0530

    Update Question5.cpp

--- Question5.cpp
+++ Question5.cpp
@@ -1,4 +1,4 @@
-//PROGRAM TO FIND LCM OF 1 TO 20 
+//PROGRAM TO FIND LCM OF 1 TO 20
 #include<iostream>
 using namespace std;
 #include<conio.h>
@@ -25,5 +25,4 @@ int main()
 	int arr[]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
 	cout<<lcm(arr);
 	getch();
-	
 }
